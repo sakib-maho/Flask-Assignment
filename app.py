58c3ea71ab9897122b2521893a5c899394304b4d
msakib162004 <70893762+msakib162004@users.noreply.github.com>
2021-12-10 18:33:27 +0600

    Update app.py

--- app.py
+++ app.py
@@ -84,10 +84,6 @@ def un():
     return data
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
